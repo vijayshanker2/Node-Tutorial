a07e82a67963812ede3d3e655ee85bb211746b85
MURALEEDHARAN Vijay Shanker <V9N@dsone.3ds.com>
2021-06-04 17:11:02 +0530

    53.47

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 7
+
+if (amount < 10) {
+    console.log('Small Number')
+}
+else {
+    console.log('Large Number')
+}
+
+console.log('Hey!! This is my first node app')
---
+++ 2-Globals.js
@@ -0,0 +1,8 @@
+
+console.log(__dirname)
+
+let interval = setInterval(()=> console.log("Hello World !!!   "),1000)
+clearInterval(interval)
+
+console.log(__filename)
+
