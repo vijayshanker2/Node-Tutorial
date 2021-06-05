516ddbae83e7524cfb73b99fadc32afc179cfbc5
MURALEEDHARAN Vijay Shanker <V9N@dsone.3ds.com>
2021-06-05 14:36:43 +0530

    log

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,5 +1,5 @@
 const {readFileSync,writeFileSync} = require('fs')
-
+console.log('start')
 const firstFile = readFileSync('./content/first.txt','utf8')
 const secondFile = readFileSync('./content/second.txt','utf8')
 
@@ -7,3 +7,5 @@ console.log(firstFile)
 console.log(secondFile)
 
 writeFileSync('./content/result-sync.txt',`Here is the result \n${firstFile} \n${secondFile}`,{flag:'a'})
+
+console.log('Done with this task \nStart with new task')
